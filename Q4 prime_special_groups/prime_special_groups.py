c4289a448465ec5782a5eb3c6ab7d69d7282b1df
alessmcs <alessandramancas23@gmail.com>
2024-04-22 21:38:28 -0400

    ajouté construction du graphe

--- Q4 prime_special_groups/prime_special_groups.py
+++ Q4 prime_special_groups/prime_special_groups.py
@@ -77,179 +77,90 @@ def nextPrime(n):
         isPrime = BTest(i, 4)
     return i #the next prime
 
-# TODO: tester si a et b (nombres premiers) forment une paire spéciale 
+# tester si a et b forment une paire spéciale
 def testPaireSpeciale(a,b):
-    # tester toutes les permutations possibles entre les 2 nombres et voir si chaque résultat est un nombre premier 
-    # utiliser la fonction pour generer les permutations quon a fait dans le devoir 2 
-    # & check pour chaque permutation si elle est premiere
-    # si une perm n'est pas première, return false direct ce n'est pas une paire spéciale 
-    permutations = generate_permutation(int(str(a) + str(b)))
-    for p in permutations: 
-        totalString = '' 
-        for i in p: 
-            totalString += i
-        
-        if not BTest(int(totalString), 4): 
-            return False # ce n'est pas une paire spéciale 
+    if BTest(int(str(a) + str(b)),4) and BTest(int(str(b) + str(a)), 4):
+        return  True
+    else:
+        return False
 
-    # c'est une paire spéciale
-    return True
+def deep_first_search(node, clique, adjacence, visited) :
+    visited.add(node)
+    clique.add(node)
+    for voisin in adjacence[node] :
+        if voisin not in visited :
+            deep_first_search(voisin, clique, adjacence, visited)
 
-# selon la fonction en c++ qu'on a implémenté dans le devoir 2
-# https://stackoverflow.com/questions/4223349/python-implementation-for-next-permutation-in-stl
+def clique_4_premier(adjacence) :
+    cliques = []
+    visited = set()
 
-# TODO: fix permutation 
-def generate_permutation(n):
-    premier_str = str(n)
+    for node in adjacence :
+        if node not in visited :
+            clique = set()
+            deep_first_search(node, clique, adjacence, visited)
+            if len(clique) == 4:
+                cliques.append(clique)
 
-    tab_premier = []
-    for letter in premier_str:
-        tab_premier.append(letter)
+    return cliques
 
-    permutations = [list(tab_premier)]  
+# https://fr.wikipedia.org/wiki/Tri_fusion#Algorithme
+def triFusion(L):
+    if len(L) == 1:
+        return L
+    else:
+        return fusion( triFusion(L[:len(L)//2]) , triFusion(L[len(L)//2:]) )
 
-    while next_permutation(tab_premier):
-        permutations.append(list(tab_premier))  
-        
-    return permutations
+def fusion(A,B):
+    if len(A) == 0:
+        return B
+    elif len(B) == 0:
+        return A
+    elif A[0] <= B[0]:
+        return [A[0]] + fusion( A[1:] , B )
+    else:
+        return [B[0]] + fusion( A , B[1:] )
 
-# https://pythonadventures.wordpress.com/2012/03/21/lexicographically-next-permutation/ 
-# https://www.wikiwand.com/en/Permutation#Generation_in_lexicographic_order 
-def next_permutation(tab):
 
-    i = len(tab) - 2
-    # find the largest index k such that tab[k] < tab[k+1]
-    while not (i < 0 or tab[i] < tab[i+1]):
-        i -= 1
-    if i < 0:
-        return False # if no such index return last permutation 
+def trier_ensembles_sommes(cliques, n) :
+    global answer
+    dict = {}
+    somme = []
+    for clique in cliques :
+        somme.append(sum(clique))
+        dict[sum(clique)] = clique
 
-    j = len(tab) - 1
-    # find the largest value k such that tab[k] < tab[l] 
-    while not (tab[j] > tab[i]):
-        j -= 1
-    # swap the value of tab[i] with tab[j]
-    tab[i], tab[j] = tab[j], tab[i]
+    somme_triee = triFusion(somme)
 
-    # reverse the sequence from tab[i+1] up to the end 
-    tab[i+1:] = reversed(tab[i+1:])
-    return True
+    return somme_triee[n]
 
 def main(args):
-    #n = int(args[0])
-    #output_file = args[1]
-
-    # 1 <= n <= 100
-    # TODO: calculer la sommme du n-ème ensemble spécial
-
-    # donc :
-    # tab = generate_permutation(123)
-    # print(tab)
-
-
-    # k = 4; 
- 
-    # print("All primes smaller than 100: ")
-    # for n in range(1,100):
-    #     if (BTest(n, k)):
-    #        print(n , end=" ")
-
-    # regarder tous les nombres premiers
-    solution = False 
-    n = 3
-    edges = []
-    adj = [0]*3
-    
-    # # on trouve un nombre premier 
-    # # on l'ajoute au graphe en le testant avec tous les autres nombres premiers pour add des arêtes
     # # on check pour clique 
     # # continue until desired solution found: un ensemble spécial 
-    primes = [3]
+
+    # construction du graphe
+    edges = [] ; adj = {}
+    n = 3 ; adj[3] = [] ; primes = [3]
+    solution = False
+    
     while not solution: 
         b = nextPrime(n)
-        primes.append
+        primes.append(b)
+        adj[b] = []
         # voir la relation de ce nouveau nb premier avec les autres dans le graphe 
-        # TODO: add every prime to the graph or only the ones that are special??
         for p in primes:
-            if testPaireSpeciale(p,b):
+            if testPaireSpeciale(p,b) and p != b:
                 # ajouter l'arête
                 edges.append([b,p])
                 edges.append([p,b])
+
+                adj[b].append(p)
                 adj[p].append(b)
             # TODO; après avoir add le nombre, check s'il y a une clique speciale 
             n = b
         else:  # si n pas premier, passer au prochain 
             n += 1
 
-    print(testPaireSpeciale(67, 3))
-    # TODO: fix test paire speciale bc its not working apparentlty
-
-    
-    # TODO: find a smarter way to calculate primes so you dont have to start at 0 every time 
-    
-    # les mettre dans la liste d'adjacence (créer une arête si 2 nombres font une paire spéciale - donc faire un test pour voir s'ils forment une paire speciale )
-    #  https://www.geeksforgeeks.org/print-adjacency-list-for-a-directed-graph/ 
-
-
-
-
-
-
-    #answer = 0
-
-    # answering
-    #write(output_file, str(answer))
-    return
-
-    def deep_first_search(node, clique, adjacence, visited) :
-        visited.add(node)
-        clique.add(node)
-        for voisin in adjacence[node] :
-            if voisin not in visited :
-                deep_first_search(voisin, clique, adjacence, visited)
-
-    def clique_4_premier(adjacence) :
-        cliques = []
-        visited = set()
-
-        for node in adjacence :
-            if node not in visited :
-                clique = set()
-                deep_first_search(node, clique, adjacence, visited)
-                if len(clique) == 4:
-                    cliques.append(clique)
-
-        return cliques
-
-    # https://fr.wikipedia.org/wiki/Tri_fusion#Algorithme
-    def triFusion(L):
-        if len(L) == 1:
-            return L
-        else:
-            return fusion( triFusion(L[:len(L)//2]) , triFusion(L[len(L)//2:]) )
-
-    def fusion(A,B):
-        if len(A) == 0:
-            return B
-        elif len(B) == 0:
-            return A
-        elif A[0] <= B[0]:
-            return [A[0]] + fusion( A[1:] , B )
-        else:
-            return [B[0]] + fusion( A , B[1:] )
-
-
-    def trier_ensembles_sommes(cliques, n) :
-        global answer
-        dict = {}
-        somme = []
-        for clique in cliques :
-            somme.append(sum(clique))
-            dict[sum(clique)] = clique
-
-        somme_triee = triFusion(somme)
-
-        return somme_triee[n]
     
 
 # NE PAS TOUCHER
