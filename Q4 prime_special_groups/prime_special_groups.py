e98c1670420778204608b10d31c32a84923cbecc
alessmcs <alessandramancas23@gmail.com>
2024-04-23 20:37:34 -0400

    update q4

--- Q4 prime_special_groups/prime_special_groups.py
+++ Q4 prime_special_groups/prime_special_groups.py
@@ -92,7 +92,7 @@ def deep_first_search(node, clique, adjacence, visited) :
             deep_first_search(voisin, clique, adjacence, visited)
 
 
-def clique_4_premier(adjacence, cliquesExistantes) :
+# def clique_4_premier(adjacence, cliquesExistantes) :
 
     # def etendre_clique(cliqueActuelle):
     #     if len(cliqueActuelle) == 4:
@@ -108,22 +108,61 @@ def clique_4_premier(adjacence, cliquesExistantes) :
     # for node in adjacence:
     #     etendre_clique(adjacence[node])
     # return cliques
-    
-    #   FONCTION ORIGINAL DONT DELETE 
-    cliques = [set(c) for c in cliquesExistantes]
-    visited = set()
 
-    for node in adjacence :
-        if node not in visited :
-            print(node)
-            clique = set()
-            deep_first_search(node, clique, adjacence, visited)
-            if len(clique) == 4 and clique not in cliquesExistantes:
-                special_pair = all(testPaireSpeciale(a, b) for a in clique for b in clique if a != b)
-                if special_pair:
-                    cliques.append(clique)
-                    visited.add(node)
-    return cliques
+    # code d'info theo figure it out tmr 
+    
+# def trouver4clique(adj):
+#     noeudsValides = []
+#     for node in adj:
+#         # on va seulement regarder les noeuds qui ont au moins 3 voisins, car ceux qui ont moins seront pas dans la 4-clique
+#         if len(adj[node]) >= 3: 
+#             noeudsValides.append(node)
+    
+#     for node in adj:
+#         cliqueTrouvee = set()
+#         for i in adj[node]:
+
+
+def cliqueBacktrack(cliquesExistantes, graphe, noeudActuel, cliqueActuelle, visited):
+    clique = cliqueActuelle[:]
+    cliques = cliquesExistantes[:]
+    v = visited.copy()
+    # si le noeud actual a moins de 3 voisins, return 
+    if len(graphe[noeudActuel]) < 3:
+        return cliques, v
+    
+    v.add(noeudActuel)
+    # check les voisins 
+    n = 0
+    for i in graphe[noeudActuel]:
+        if i in cliqueActuelle:
+            n += 1
+    
+    # si tous les elems de la clique actuelle se trouvent dans la liste, 
+    # ajouter a la clique actuelle & visiter le prochain noeud qui n'est pas dans la clique 
+    if n == len(cliqueActuelle):
+        clique.append(noeudActuel)
+    if len(clique) == 4:
+        # une fois qu'on a trouvé une clique de taille 4, verifier si c'est special
+        special = True
+        for i in clique:
+            for j in clique:
+                if i != j and testPaireSpeciale(i,j) == False:
+                    special = False
+        if special == True and clique not in cliques:
+            cliques.append(set(clique))
+        return cliques, v
+    else: 
+        for i in graphe[noeudActuel]:
+            if i not in clique and i not in v:
+                cliquesNouv, vNouv = cliqueBacktrack(cliques, graphe, i, clique[:], v)
+                cliques = cliquesNouv
+                v = vNouv
+                # we only swim up every time a 4-clique is found, so update cliquesExistantes for the current instance and keep searching 
+    return cliques, v
+    
+
+
 
 # https://fr.wikipedia.org/wiki/Tri_fusion#Algorithme
 def triFusion(L):
@@ -145,11 +184,9 @@ def fusion(A,B):
 
 def trier_ensembles_sommes(cliques) :
     global answer
-    dict = {}
     somme = []
     for clique in cliques :
         somme.append(sum(clique))
-        dict[sum(clique)] = clique
 
     somme_triee = triFusion(somme)
 
@@ -167,19 +204,24 @@ def main(args):
     #     6 : [5]
     # }
     # cliquesExistantes = []
-    # cliquesTest = clique_4_premier(adjacence, cliquesExistantes)
+    # cliquesTest = cliqueBacktrack(cliquesExistantes, adjacence, 1, [1], {1})
     # print(trier_ensembles_sommes(cliquesTest)[0])
 
     # # on check pour clique 
     # # continue until desired solution found: un ensemble spécial 
-    neme_elem = 1
+    neme_elem = 3
     # construction du graphe
     edges = [] ; adj = {}
     n = 3 ; adj[3] = [] ; primes = [3]
     reponse = 0
     solution = False
     cliquesExistantes = []
+    cliquesUpdated = []
     listeSommes = []
+    visited = set()
+    visited.add(3)
+
+    # rouler le problème 
     while not solution: 
         b = nextPrime(n)
         primes.append(b)
@@ -187,28 +229,43 @@ def main(args):
         # voir la relation de ce nouveau nb premier avec les autres dans le graphe 
         for p in primes:
             if testPaireSpeciale(p,b) and p != b:
-                # ajouter l'arête
+                # ajouter l'arête 
+                if b == 1871:
+                    print('yes2') 
                 edges.append([b,p])
                 edges.append([p,b])
 
                 adj[b].append(p)
                 adj[p].append(b)
-                # trouve toutes les cliques de taille 4 une fois qu'on a ajouté un nouveau sommet/arêtes 
-                cliquesUpdated = clique_4_premier(adj,cliquesExistantes)
-                if len(cliquesExistantes) < len(cliquesUpdated):
-                    cliquesExistantes = cliquesUpdated
-                    print(cliquesExistantes)
-                    # retrier l'ensemble des sommes
-                    listeSommes = trier_ensembles_sommes(cliquesExistantes)
+        # trouve toutes les cliques de taille 4 une fois qu'on a ajouté un nouveau sommet/arêtes 
+        if b == 827:
+            print('yes') 
+        if b == 1871:
+            print('yes2')      
+        cliquesUpdated, visited = cliqueBacktrack([], adj, b, [b], {b})
+        
+        # on a trouvé une clique
+        if cliquesUpdated not in cliquesExistantes:
+            if len(cliquesExistantes) == 0:
+                cliquesExistantes = cliquesUpdated
+            elif len(cliquesUpdated) > 0: 
+                cliquesExistantes.append(cliquesUpdated[0])
+            print(len(cliquesExistantes))
+        
+        # faire l'ensemble des sommes
+        if len(cliquesExistantes) == 100:
+            listeSommes = trier_ensembles_sommes(cliquesExistantes)
+            reponse = listeSommes[neme_elem-1]
+            solution = True
+
+        n = b         
                 # regarder toutes les cliques d'avant et si on a add une clique, calculer sa somme et l'ajouter à la liste de sommes 
                 # check w/taille de la clique 
-                if len(listeSommes) == neme_elem:
-                    reponse = listeSommes[neme_elem]
-                    solution = True 
-            n = b
-        else:  # si n pas premier, passer au prochain 
-            n += 1
+        # if len(listeSommes) == neme_elem:
+        #     reponse = listeSommes[neme_elem]
+        #     solution = True 
 
+            
 
     
     print(reponse)
